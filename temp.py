0e70fd3130eab1d447826412ae46a2f7205f1c4e
Mike Jovanovich <mpjovanovich@gmail.com>
2026-07-15 01:39:48 -0400

    it works yay

--- temp.py
+++ temp.py
@@ -1,8 +1,18 @@
 from random import Random
+from PrimitiveAlphabetBuilder import PrimitiveAlphabetBuilder
 from World import World
 from WorldConfig import WorldConfig
 from WorldFactory import WorldFactory
 
 rng = Random()
-world = WorldFactory.create(WorldConfig(dimensions=(10,), primitive_count=3, population_size=5, random_number_generator=rng))
+config = WorldConfig(
+    dimensions=(10,), 
+    primitive_count=3, 
+    population_size=5, 
+    random_number_generator=rng
+)
+
+alphabet_builder = PrimitiveAlphabetBuilder()
+world = WorldFactory.create(config, alphabet_builder)
+
 print(world.to_string())
